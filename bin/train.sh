5d07c612d7f218793679b26434699d36e1087b26
Emma Strubell <emma.strubell@gmail.com>
2018-07-16 13:45:05 -0400

    train scriopt

--- bin/train.sh
+++ bin/train.sh
@@ -1,1 +1,13 @@
 #!/usr/bin/env bash
+
+save_dir=$1
+
+train_file=$DATA_DIR/conll05st-release-new/train-set.gz.parse.sdeps.combined.bio
+dev_file=$DATA_DIR/conll05st-release-new/dev-set.gz.parse.sdeps.combined.bio
+embeddings_file=embeddings/glove.6B.100d.txt
+
+srun --gres=gpu --partition=gpu python3 train.py \
+--train_file $train_file \
+--dev_file $dev_file \
+--save_dir $save_dir \
+--word_embedding_file $embeddings_file
