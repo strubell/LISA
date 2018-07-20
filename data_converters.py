0627c7d789c9e3939a39584d4b9244fd0567501d
Emma Strubell <emma.strubell@gmail.com>
2018-07-19 21:12:53 -0400

    fix

--- data_converters.py
+++ data_converters.py
@@ -22,6 +22,7 @@ def conll12_binary_predicates_converter(split_line, idx):
 
 def joint_converter(split_line, idx, component_converters):
   components = [dispatch(converter)(split_line, i) for i, converter in zip(idx, component_converters)]
+  print(components)
   return [constants.JOINT_LABEL_SEP.join(components)]
 
 
