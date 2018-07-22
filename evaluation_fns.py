6e8adb3a73fcc70474f358a67f9b17dc30aa5b6b
Emma Strubell <emma.strubell@gmail.com>
2018-07-22 14:46:22 -0400

    fix

--- evaluation_fns.py
+++ evaluation_fns.py
@@ -5,10 +5,10 @@ def accuracy(preds, targets, tokens_to_keep):
   return tf.metrics.accuracy(targets, preds, weights=tokens_to_keep)
 
 
-
 dispatcher = {
   'accuracy': accuracy,
 }
+
 
 def dispatch(fn_name):
   try:
@@ -17,10 +17,12 @@ def dispatch(fn_name):
     print('Undefined evaluation function `%s' % fn_name)
     exit(1)
 
+
 def get_params(predictions, task_map, train_outputs, task_labels, tokens_to_keep):
   params = {'predictions': predictions, 'targets': task_labels, 'tokens_to_keep': tokens_to_keep}
-  params_map = task_map['params']
-  for param_name, param_values in params_map.items():
-    outputs_layer = train_outputs[param_values['layer']]
-    params[param_name] = outputs_layer[param_values['output']]
+  if 'params' in task_map:
+    params_map = task_map['params']
+    for param_name, param_values in params_map.items():
+      outputs_layer = train_outputs[param_values['layer']]
+      params[param_name] = outputs_layer[param_values['output']]
   return params
