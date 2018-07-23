fa3ca38735a8f2d60fc9d2ee3357051b82915d4b
Emma Strubell <emma.strubell@gmail.com>
2018-07-23 15:26:12 -0400

    model

--- model.py
+++ model.py
@@ -176,7 +176,7 @@ class LISAModel:
       #                            global_step=tf.train.get_global_step())
       # train_op = optimizer.minimize(loss=loss)
 
-      items_to_log['learning_rate'] = optimizer.learning_rate
+      # items_to_log['learning_rate'] = optimizer.learning_rate
 
 
       # preds = tf.argmax(scores, -1)
