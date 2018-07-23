723abeac60e00af82cd6e34d7764a4ac86397de7
Emma Strubell <emma.strubell@gmail.com>
2018-07-23 14:29:35 -0400

    switch back to adam and add in srl

--- model.py
+++ model.py
@@ -164,16 +164,16 @@ class LISAModel:
       nu = 0.98
       epsilon = 1e-12
 
-      # optimizer = tf.contrib.opt.LazyAdamOptimizer(learning_rate=0.0001, beta1=mu, beta2=0.999, epsilon=epsilon)
-      # gradients, variables = zip(*optimizer.compute_gradients(loss))
-      # gradients, _ = tf.clip_by_global_norm(gradients, gradient_clip_norm)
-      # train_op = optimizer.apply_gradients(zip(gradients, variables), global_step=tf.train.get_global_step())
-      # # train_op = optimizer.minimize(loss=loss)
+      optimizer = tf.contrib.opt.LazyAdamOptimizer(learning_rate=0.0001, beta1=mu, beta2=0.999, epsilon=epsilon)
+      gradients, variables = zip(*optimizer.compute_gradients(loss))
+      gradients, _ = tf.clip_by_global_norm(gradients, gradient_clip_norm)
+      train_op = optimizer.apply_gradients(zip(gradients, variables), global_step=tf.train.get_global_step())
+      # train_op = optimizer.minimize(loss=loss)
 
-      optimizer = RadamOptimizer(learning_rate=learning_rate, mu=mu, nu=nu, epsilon=epsilon, decay_rate=decay_rate,
-                                 warmup_steps=warmup_steps, gradient_clip_norm=gradient_clip_norm,
-                                 global_step=tf.train.get_global_step())
-      train_op = optimizer.minimize(loss=loss)
+      # optimizer = RadamOptimizer(learning_rate=learning_rate, mu=mu, nu=nu, epsilon=epsilon, decay_rate=decay_rate,
+      #                            warmup_steps=warmup_steps, gradient_clip_norm=gradient_clip_norm,
+      #                            global_step=tf.train.get_global_step())
+      # train_op = optimizer.minimize(loss=loss)
 
       items_to_log['learning_rate'] = optimizer.learning_rate
 
