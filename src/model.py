fix moving average

src/model.py
@@ -268,13 +268,14 @@ class LISAModel:
                 # export_outputs['%s_predict' % task] = predict_output
 
       # use moving averages of variables if evaluating
-      moving_averager = tf.train.ExponentialMovingAverage(hparams.moving_average_decay, zero_debias=True)
-      moving_average_op = moving_averager.apply(tf.trainable_variables())
-      tf.logging.log(tf.logging.INFO,
-                     "Using moving average for variables: %s" % str([v for v in tf.GraphKeys.TRAINABLE_VARIABLES]))
-      tf.add_to_collection(tf.GraphKeys.UPDATE_OPS, moving_average_op)
-      assign_dep = tf.cond(mode != ModeKeys.TRAIN, lambda: nn_utils.set_vars_to_moving_average(moving_averager),
-                                           lambda: tf.no_op())
+      assign_dep = tf.no_op()
+      if mode != ModeKeys.TRAIN:
+        moving_averager = tf.train.ExponentialMovingAverage(hparams.moving_average_decay, zero_debias=True)
+        moving_average_op = moving_averager.apply(tf.trainable_variables())
+        tf.logging.log(tf.logging.INFO,
+                       "Using moving average for variables: %s" % str([v.name for v in tf.trainable_variables()]))
+        tf.add_to_collection(tf.GraphKeys.UPDATE_OPS, moving_average_op)
+        assign_dep = nn_utils.set_vars_to_moving_average(moving_averager)
 
       with tf.control_dependencies([assign_dep]):
 