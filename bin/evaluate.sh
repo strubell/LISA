aa48bb1a97f23417ef06bd4122ff7c106c0298ae
Emma Strubell <emma.strubell@gmail.com>
2018-08-17 16:42:11 -0400

    run script

---
+++ bin/evaluate.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+config_file=$1
+
+source ${config_file}
+
+params=${@:2}
+
+transition_stats=$data_dir/transition_probs.tsv
+
+python3 src/evaluate.py \
+--test_files $test_files \
+--dev_files $dev_files \
+--transition_stats $transition_stats \
+--data_config $data_config \
+--model_configs $model_configs \
+--task_configs $task_configs \
+--layer_configs $layer_configs \
+--attention_configs $attention_configs \
+$params
+
