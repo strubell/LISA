511297cc44b1d3ff4970dbf65a1e9132e13ec841
Emma Strubell <emma.strubell@gmail.com>
2018-09-29 14:18:34 -0400

    tokenize

--- bin/preprocess-wmt.py
+++ bin/preprocess-wmt.py
@@ -1,4 +1,5 @@
 import argparse
+import time
 from nltk.tokenize.nist import NISTTokenizer
 
 arg_parser = argparse.ArgumentParser()
@@ -12,6 +13,8 @@ tokenizer = NISTTokenizer()
 source_max_len = 0
 target_max_len = 0
 target_2x_source = 0
+line_count = 0
+start_time = time.time()
 with open(args.source_file, 'r') as source, open(args.target_file, 'r') as target:
   for source_line, target_line in zip(source, target):
     source_toks = tokenizer.tokenize(source_line)
@@ -25,7 +28,12 @@ with open(args.source_file, 'r') as source, open(args.target_file, 'r') as targe
 
     if target_len > 2 * source_len:
       target_2x_source += 1
+    line_count += 1
 
+    if line_count % 10000 == 0:
+      print("Processed %d lines" % line_count)
+
+print("Processed %d lines in %d seconds" % (line_count, time.time() - start_time))
 print("max source len: %d" % source_max_len)
 print("max target len: %d" % target_max_len)
 print("target > 2 * source: %d" % target_2x_source)
