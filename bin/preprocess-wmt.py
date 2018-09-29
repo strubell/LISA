c9986ab112132417b2f997886b4399ffb1294959
Emma Strubell <emma.strubell@gmail.com>
2018-09-29 14:45:49 -0400

    print

--- bin/preprocess-wmt.py
+++ bin/preprocess-wmt.py
@@ -5,20 +5,26 @@ from nltk.tokenize.nist import NISTTokenizer
 arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument('--source_file', type=str)
 arg_parser.add_argument('--target_file', type=str)
+arg_parser.add_argument('--output_file', type=str)
 
 args = arg_parser.parse_args()
 
 tokenizer = NISTTokenizer()
+
+PAD_STR = "<PAD>"
 
 source_max_len = 0
 target_max_len = 0
 target_2x_source = 0
 line_count = 0
 start_time = time.time()
-with open(args.source_file, 'r') as source, open(args.target_file, 'r') as target:
+with open(args.source_file, 'r') as source, \
+      open(args.target_file, 'r') as target, \
+      open(args.output_file, 'w') as output:
   for source_line, target_line in zip(source, target):
     source_toks = tokenizer.tokenize(source_line)
     target_toks = tokenizer.tokenize(target_line)
+
     source_len = len(source_toks)
     target_len = len(target_toks)
     if source_len > source_max_len:
@@ -28,8 +34,16 @@ with open(args.source_file, 'r') as source, open(args.target_file, 'r') as targe
 
     if target_len > 2 * source_len:
       target_2x_source += 1
-    line_count += 1
+    else:
+      if source_len < target_len:
+        source_toks += [PAD_STR] * (target_len - source_len)
+      elif source_len > target_len:
+        target_toks += [PAD_STR] * (source_len - target_len)
 
+    for source_tok, target_tok in zip(source_toks, target_toks):
+      print("%s\t%s" % (source_tok, target_tok), file=output)
+
+    line_count += 1
     if line_count % 10000 == 0:
       print("Processed %d lines" % line_count)
 
