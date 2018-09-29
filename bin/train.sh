1a08bfb264557731808f3a37ebb9d45a4b200f03
Emma Strubell <emma.strubell@gmail.com>
2018-09-29 15:44:39 -0400

    fix train script for optional attn configs

--- bin/train.sh
+++ bin/train.sh
@@ -8,6 +8,12 @@ params=${@:2}
 
 transition_stats=$data_dir/transition_probs.tsv
 
+# attention_configs is optional
+attention_config_param="--attention_configs $attention_configs"
+if [ "$attention_configs" == "" ]; then
+    attention_config_param=""
+fi
+
 python3 src/train.py \
 --train_files $train_files \
 --dev_files $dev_files \
@@ -16,6 +22,6 @@ python3 src/train.py \
 --model_configs $model_configs \
 --task_configs $task_configs \
 --layer_configs $layer_configs \
---attention_configs $attention_configs \
+$attention_config_param \
 $params
 
