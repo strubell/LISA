6213d675474b483363c6a54b3a7ec48963c4fe8e
Emma Strubell <emma.strubell@gmail.com>
2018-12-17 17:40:49 -0500

    debug

--- src/evaluate_exported.py
+++ src/evaluate_exported.py
@@ -126,8 +126,8 @@ with tf.Session() as sess:
 
   print(predictions.keys())
 
-  srl_predictions = predictions['srl_predictions'][0]
-  predicate_predictions = predictions['joint_pos_predicate_predicate_predictions'][0]
+  srl_predictions = predictions['srl_predictions']
+  predicate_predictions = predictions['joint_pos_predicate_predicate_predictions']
 
   feats = {f: input_np[:, :, idx] for f, idx in feature_idx_map.items()}
 
