d5d95248d1ad0fa3c59819f438299c91fb565157
Emma Strubell <emma.strubell@gmail.com>
2018-12-17 17:45:59 -0500

    fix

--- src/evaluation_fns.py
+++ src/evaluation_fns.py
@@ -16,7 +16,7 @@ def convert_bilou(bio_predicted_roles):
   converted = []
   started_types = []
   for i, s in enumerate(bio_predicted_roles):
-    s = s.decode('utf-8')
+    s = s if isinstance(s, str) else s.decode('utf-8')
     label_parts = s.split('/')
     curr_len = len(label_parts)
     combined_str = ''
