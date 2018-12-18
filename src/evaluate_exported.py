0478675fd707d337cbc947103f2361037d602a65
Emma Strubell <emma.strubell@gmail.com>
2018-12-18 15:07:54 -0500

    fix

--- src/evaluate_exported.py
+++ src/evaluate_exported.py
@@ -145,9 +145,7 @@ def eval_fn(input_op, sess):
 
       print(predictions[0].keys())
 
-      print(layer_task_config)
-
-      if i in layer_task_config:
+      for i in layer_task_config:
         for task, task_map in layer_task_config[i].items():
           for eval_name, eval_map in task_map['eval_fns'].items():
             print(task, eval_map['name'])
