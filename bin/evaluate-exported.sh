fc77764bb4007e771e6e478aacac703fd73f9dc6
Emma Strubell <emma.strubell@gmail.com>
2018-12-18 11:11:12 -0500

    add test to eval script

--- bin/evaluate-exported.sh
+++ bin/evaluate-exported.sh
@@ -11,6 +11,7 @@ echo "Using CUDA_VISIBLE_DEVICES="$CUDA_VISIBLE_DEVICES
 transition_stats=$data_dir/transition_probs.tsv
 
 python3 src/evaluate_exported.py \
+--test_files $test_files \
 --dev_files $dev_files \
 --transition_stats $transition_stats \
 --data_config $data_config \
