cc4ab3736a8ca51f4cafde172d3d10f35d102117
Emma Strubell <emma.strubell@gmail.com>
2018-12-21 20:57:20 -0500

    accumulators

--- src/dataset.py
+++ src/dataset.py
@@ -64,10 +64,10 @@ def get_data_iterator(data_filenames, data_config, vocab_lookup_ops, batch_size,
 
     # shuffle and expand out epochs if training
     if shuffle:
-      # dataset = dataset.apply(tf.contrib.data.shuffle_and_repeat(buffer_size=batch_size*shuffle_buffer_multiplier,
-      #                                                            count=num_epochs))
-      dataset = dataset.apply(tf.contrib.data.shuffle_and_repeat(buffer_size=batch_size,
+      dataset = dataset.apply(tf.contrib.data.shuffle_and_repeat(buffer_size=batch_size*shuffle_buffer_multiplier,
                                                                  count=num_epochs))
+      # dataset = dataset.apply(tf.contrib.data.shuffle_and_repeat(buffer_size=batch_size,
+      #                                                            count=num_epochs))
 
     # todo should the buffer be bigger?
     dataset.prefetch(buffer_size=1)
