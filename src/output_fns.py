018731653bdb29cd813b12743aa1cc8a3cd2d4f3
Emma Strubell <emma.strubell@gmail.com>
2019-05-02 13:45:55 -0400

    fix

--- src/output_fns.py
+++ src/output_fns.py
@@ -313,9 +313,10 @@ def get_params(mode, model_config, task_map, train_outputs, features, current_ou
       params[param_name] = {map_name: joint_lookup_maps[map_name] for map_name in param_values['joint_maps']}
     elif 'label' in param_values:
       if mode == ModeKeys.PREDICT:
-        util.fatal_error("Labels can't be used during prediction (tried to pass '%s=%s' in output_fn)" %
-                         (param_name, param_values['label']))
-      params[param_name] = labels[param_values['label']]
+        tf.logging.warn("Labels can't be used during prediction (passing '%s=None' in output_fn)" % param_name)
+        params[param_name] = None
+      else:
+        params[param_name] = labels[param_values['label']]
     elif 'feature' in param_values:
       params[param_name] = features[param_values['feature']]
     # otherwise, this is a previous-prediction-type param, look those up and pass through
